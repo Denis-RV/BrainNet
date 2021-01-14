72a0272b92320892ef761cd37a721504d500d02e
Denis <ryabchickov20@gmail.com>
2021-01-14 15:39:04 +0300

    Test

--- gitSync.sh
+++ gitSync.sh
@@ -3,5 +3,5 @@ git pull git@github.com:Denis-VR/BrainNet.git
 
 git add *
 git commit -m "$1"
-git push git@github.com:Denis-VR/BrainNet.git obsidian_git
+git push git@github.com:Denis-VR/BrainNet.git master
 
