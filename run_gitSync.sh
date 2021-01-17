2a00bc9169397a9adae16a0a94fcd0f1f58b1cfb
Denis <ryabchickov20@gmail.com>
2021-01-17 04:39:05 +0300

    Sun Jan 17 04:39:05 MSK 2021

--- run_gitSync.sh
+++ run_gitSync.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-#/home/danis/Documents/Obsidian/All/gitSync.sh 
-cd /home/danis/Documents/Obsidian/All/
-git pull git@github.com:Denis-VR/BrainNet.git
+#Синхронизация с 
+#cd /home/danis/Documents/Obsidian/All/
+/home/danis/Documents/Obsidian/All/gitSync.sh &
 
-git add *
-git commit -m "$(date)"
-git push git@github.com:Denis-VR/BrainNet.git master
+# git pull git@github.com:Denis-VR/BrainNet.git
+
+# git add *
+# git commit -m "$(date)"
+# git push git@github.com:Denis-VR/BrainNet.git master
 
 #Запуск Obsidian
 /usr/bin/flatpak run --branch=stable --arch=x86_64 --command=obsidian.sh --file-forwarding md.obsidian.Obsidian @@u %u @@
 
-#/home/danis/Documents/Obsidian/All/gitSync.sh 
-#cd /home/danis/Documents/Obsidian/All/
-pwd
-git pull git@github.com:Denis-VR/BrainNet.git
+/home/danis/Documents/Obsidian/All/gitSync.sh 
 
-git add *
-git commit -m "$(date)"
-git push git@github.com:Denis-VR/BrainNet.git master
+
+#git pull git@github.com:Denis-VR/BrainNet.git
+
+#git add *
+#git commit -m "$(date)"
+#git push git@github.com:Denis-VR/BrainNet.git master
